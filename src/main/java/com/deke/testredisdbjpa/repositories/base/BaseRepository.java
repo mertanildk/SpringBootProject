2042af572660a5624439b89b79c665ac78f28803
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-09 21:27:54 +0300

    Spesification
    Added JpaSpesificationExecutor

--- src/main/java/com/deke/testredisdbjpa/repositories/base/BaseRepository.java
+++ src/main/java/com/deke/testredisdbjpa/repositories/base/BaseRepository.java
@@ -2,11 +2,12 @@ package com.deke.testredisdbjpa.repositories.base;
 
 import com.deke.testredisdbjpa.entity.baseEntity.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Component("baseRepository")
-public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, String>  {
+public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, String>, JpaSpecificationExecutor<T> {
 }
 
