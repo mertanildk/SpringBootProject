b1cdeea978c1d08d12f8a5de7c29195a3a46213c
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-10 20:36:03 +0300

    Create CustomerSearchDto.java
    Created CustomerSearchDto

---
+++ src/main/java/com/deke/testredisdbjpa/dto/search/CustomerSearchDto.java
@@ -0,0 +1,11 @@
+package com.deke.testredisdbjpa.dto.search;
+
+import lombok.Data;
+
+@Data
+public class CustomerSearchDto {
+    private String name;
+    private String email;
+    private String phone;
+
+}
