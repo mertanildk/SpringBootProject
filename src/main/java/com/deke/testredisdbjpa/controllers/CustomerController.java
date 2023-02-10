0ffcbb8d437c2e935ee183b4f7ff480df7e3740b
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-10 22:45:05 +0300

    Updated Return Type CustomerController
    Updated return types and added search method.

--- src/main/java/com/deke/testredisdbjpa/controllers/CustomerController.java
+++ src/main/java/com/deke/testredisdbjpa/controllers/CustomerController.java
@@ -7,6 +7,7 @@ import com.deke.testredisdbjpa.responseApi.RestResponseEntity;
 import com.deke.testredisdbjpa.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,15 +20,19 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping("/add")
-    public ResponseEntity addCustomer(@RequestBody @Valid CustomerRequestDto customer){
+    public ResponseEntity addCustomer(@RequestBody @Valid CustomerRequestDto customer) {
         return ResponseEntity.ok(RestResponseEntity.response(customerService.addCustomer(customer)));
     }
+
     @GetMapping("/get/{id}")
-    public ResponseEntity<RestResponseEntity<Customer>> getCustomer(@PathVariable String id){
+    public ResponseEntity getCustomer(@PathVariable String id) {
         return ResponseEntity.ok(RestResponseEntity.response(customerService.getOne(id)));
     }
-    @PostMapping("/search-all-customers")
-    public ResponseEntity searchAllCustomers(@RequestBody CustomerSearchDto customerSearchDto){
+
+    @PostMapping(value = "/search-all-customers",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity searchAllCustomers(@RequestBody CustomerSearchDto customerSearchDto) {
         return ResponseEntity.ok(RestResponseEntity.response(customerService.searchAllCustomers(customerSearchDto)));
     }
 
