2b143431db88467714e800a31c7eabeee54903a0
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-12 16:34:14 +0300

    Implemented Log Aspect

--- src/main/java/com/deke/testredisdbjpa/api/controllers/CustomerController.java
+++ src/main/java/com/deke/testredisdbjpa/api/controllers/CustomerController.java
@@ -1,5 +1,7 @@
 package com.deke.testredisdbjpa.api.controllers;
 
+import com.deke.testredisdbjpa.Aop.Log;
+import com.deke.testredisdbjpa.Aop.LogType;
 import com.deke.testredisdbjpa.entity.dto.request.CustomerRequestDto;
 import com.deke.testredisdbjpa.entity.dto.search.CustomerSearchDto;
 import com.deke.testredisdbjpa.utils.responseApi.RestResponseEntity;
@@ -19,15 +21,18 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping("/add")
+    @Log(LogType.WRITE)
     public ResponseEntity addCustomer(@RequestBody @Valid CustomerRequestDto customer) {
         return ResponseEntity.ok(RestResponseEntity.response(customerService.addCustomer(customer)));
     }
 
     @GetMapping("/get/{id}")
+    @Log
     public ResponseEntity getCustomer(@PathVariable String id) {
         return ResponseEntity.ok(RestResponseEntity.response(customerService.getOne(id)));
     }
 
+    @Log(LogType.WRITE)
     @PostMapping(value = "/search-all-customers",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
