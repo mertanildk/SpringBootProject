a474c8d9d743ff995c3f03ca2f818eca0beffa98
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-12 14:44:14 +0300

    Autowired deleted RequiredArgsCo used

--- src/main/java/com/deke/testredisdbjpa/configs/cache/RedisConfig.java
+++ src/main/java/com/deke/testredisdbjpa/configs/cache/RedisConfig.java
@@ -1,6 +1,6 @@
 package com.deke.testredisdbjpa.configs.cache;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +9,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
+@RequiredArgsConstructor
 public class RedisConfig {
 
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
+    private final RedisConnectionFactory redisConnectionFactory;
 
     @Bean
     public RedisTemplate<Object,Object> redisTemplate() {
