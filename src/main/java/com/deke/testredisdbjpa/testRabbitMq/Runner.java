7568583d14009d5bb8b2392c97bea7470038d29d
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-14 22:52:09 +0300

    Changed the exception value
    Added a loop, tried to send more than one message at the same time.

--- src/main/java/com/deke/testredisdbjpa/testRabbitMq/Runner.java
+++ src/main/java/com/deke/testredisdbjpa/testRabbitMq/Runner.java
@@ -1,14 +1,10 @@
 package com.deke.testredisdbjpa.testRabbitMq;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -17,14 +13,16 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class Runner implements CommandLineRunner {
 
-
     private final RabbitTemplate rabbitTemplate;
     private final Receiver receiver;
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(RabbitMqConfigs.EXCHANGE_NAME, "foo.bar.baz", "Hello from RabbitMQ!");
-        receiver.getLatch().await(1000, TimeUnit.MILLISECONDS);
 
+    @Override
+    public void run(String... args) throws InterruptedException {
+        System.out.println("Sending message...");
+        Thread.sleep(5000);
+        for (int i = 0; i < 10; i++) {
+            rabbitTemplate.convertAndSend(RabbitMqConfigs.EXCHANGE_NAME, "foo.bar.baz", "Hello from RabbitMQ!");
+            receiver.getLatch().await(0, TimeUnit.MILLISECONDS);
+        }
     }
 }
