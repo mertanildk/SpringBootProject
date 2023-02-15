ddc321f5db44a0d530ca20e84787fe89a607ad0f
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-15 20:42:25 +0300

    Publisher class
    CommandLineRunner used. When i implement rabbitmq this class i should change the  run method

--- src/main/java/com/deke/testredisdbjpa/testRabbitMq/Runner.java
+++ src/main/java/com/deke/testredisdbjpa/testRabbitMq/Runner.java
@@ -1,28 +1,26 @@
 package com.deke.testredisdbjpa.testRabbitMq;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
 public class Runner implements CommandLineRunner {
 
+    private final String routingName = "routingKey";
+    private final String exchangeName = "exchange";
+
     private final RabbitTemplate rabbitTemplate;
-    private final Receiver receiver;
 
     @Override
     public void run(String... args) throws InterruptedException {
-        System.out.println("Sending message...");
-        Thread.sleep(5000);
-        for (int i = 0; i < 10; i++) {
-            rabbitTemplate.convertAndSend(RabbitMqConfigs.EXCHANGE_NAME, "foo.bar.baz", "Hello from RabbitMQ!");
-            receiver.getLatch().await(0, TimeUnit.MILLISECONDS);
+        System.out.println("Sending message every 2 seconds...");
+        while (true){
+            Thread.sleep(10);
+            rabbitTemplate.convertAndSend(exchangeName, routingName, "Hello World!");
         }
+
     }
 }
