5d9daddfcbb68b38841daa7758977ad9ffd12bc7
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-18 16:49:13 +0300

    @EnableMongoRepository added

--- src/main/java/com/deke/testredisdbjpa/TestRedisDbJpaApplication.java
+++ src/main/java/com/deke/testredisdbjpa/TestRedisDbJpaApplication.java
@@ -2,18 +2,16 @@ package com.deke.testredisdbjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableMongoRepositories
 public class TestRedisDbJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestRedisDbJpaApplication.class, args);
     }
-
 }
