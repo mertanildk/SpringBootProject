bc286da4ec02cb5f63f7ae2d6dd6d33ac5798b32
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-20 20:31:49 +0300

    Changed From JpaRepo to MongoRepo

--- src/main/java/com/deke/testredisdbjpa/repositories/LogRepository.java
+++ src/main/java/com/deke/testredisdbjpa/repositories/LogRepository.java
@@ -1,7 +1,7 @@
 package com.deke.testredisdbjpa.repositories;
 
 import com.deke.testredisdbjpa.entity.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface LogRepository extends JpaRepository<Log,String> {
+public interface LogRepository extends MongoRepository<Log,String> {
 }
