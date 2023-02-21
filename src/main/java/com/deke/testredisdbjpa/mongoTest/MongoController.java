b40a6cbc802e876df42f4701c2c32adb800e92c8
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-21 22:49:52 +0300

    Deleted private method and tried cache basic way

--- src/main/java/com/deke/testredisdbjpa/mongoTest/MongoController.java
+++ src/main/java/com/deke/testredisdbjpa/mongoTest/MongoController.java
@@ -5,6 +5,7 @@ import com.deke.testredisdbjpa.utils.responseApi.RestResponseEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.Cacheable;
 
 import java.util.List;
 
@@ -12,8 +13,6 @@ import java.util.List;
 @RequestMapping("/mongo")
 @RequiredArgsConstructor
 public class MongoController {
-
-
     private final UserRepository userRepository;
     private final ObjRepository phoneRepository;
 
@@ -22,25 +21,19 @@ public class MongoController {
         userRepository.save(user);
         return ResponseEntity.ok(RestResponseEntity.response(user));
     }
-
     @PostMapping("/save-phone")
     public ResponseEntity<RestResponseEntity<User>> savePhone(@RequestBody Obj phone) {
         phoneRepository.save(phone);
         return ResponseEntity.ok(RestResponseEntity.response(phone));
     }
-
     @GetMapping("/getAll")
     public ResponseEntity<RestResponseEntity<User>> getAll() {
         return ResponseEntity.ok(RestResponseEntity.response(userRepository.findAll()));
     }
-
     @GetMapping("/getAllPhone")
     @ResponseBody
+    @Cacheable("phones")
     public ResponseEntity<List<Obj>> getAllPhone() {
-        return ResponseEntity.ok(getAllPhone1());
-    }
-
-    private List<Obj> getAllPhone1() {
-        return phoneRepository.findAll();
+        return ResponseEntity.ok(phoneRepository.findAll());
     }
 }
